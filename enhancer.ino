758c4c6a05702cdf89d8687bf0e364d9d865ca43
Jordan Shaw <jrdnshw@gmail.com>
2018-01-06 18:08:12 -0500

    backup of broken class before i try a new approch...

--- enhancer.ino
+++ enhancer.ino
@@ -13,17 +13,41 @@ Mode and Median filters are recommended.
 // Libs: Adafruit_TiCoServo, Adafruit_NeoPixel, Ultrasonic, Array
 // ---------------------------------------------------------------------------
 
+// Commands
+
+// Go / Start
+// g = 103
+
+// Stop
+// s = 115
+
+// Next
+// n = 110
+
+// Previous
+// p = 112
+
+// Set config
+// c = 99
+
+
+
+#if defined(__AVR_ATtiny85__)
+ #error "This code is for ATmega boards, see other example for ATtiny."
+#endif
+
 #define DEBUG false
 
 #include <Adafruit_TiCoServo.h>
-#include <Ultrasonic.h>
-#include <SimplexNoise.h>
-#include <Array.h>
+//#include <Servo.h>
+//#include <Ultrasonic.h>
+//#include <SimplexNoise.h>
+//#include <Array.h>
 
 // NeoPixel Ring simple sketch (c) 2013 Shae Erisson
 // released under the GPLv3 license to match the rest of the AdaFruit NeoPixel library
 
-#include <Adafruit_NeoPixel.h>
+//#include <Adafruit_NeoPixel.h>
 #ifdef __AVR__
   #include <avr/power.h>
 #endif
@@ -35,135 +59,50 @@ Mode and Median filters are recommended.
 // How many NeoPixels are attached to the Arduino?
 #define NUMPIXELS      60
 
-// 
-// == Servos
-//
-Adafruit_TiCoServo servo0;  // create servo object to control a servo
-Adafruit_TiCoServo servo1;  // create servo object to control a servo
-Adafruit_TiCoServo servo2;  // create servo object to control a servo
-Adafruit_TiCoServo servo3;  // create servo object to control a servo
-Adafruit_TiCoServo servo4;  // create servo object to control a servo
-Adafruit_TiCoServo servo5;  // create servo object to control a servo
-Adafruit_TiCoServo servo6;  // create servo object to control a servo
-Adafruit_TiCoServo servo7;  // create servo object to control a servo
-Adafruit_TiCoServo servo8;  // create servo object to control a servo
-Adafruit_TiCoServo servo9;  // create servo object to control a servo
-Adafruit_TiCoServo servo10;  // create servo object to control a servo
-Adafruit_TiCoServo servo11;  // create servo object to control a servo
-Adafruit_TiCoServo servo12;  // create servo object to control a servo
-Adafruit_TiCoServo servo13;  // create servo object to control a servo
-Adafruit_TiCoServo servo14;  // create servo object to control a servo
-Adafruit_TiCoServo servo15;  // create servo object to control a servo
+#define MAX_DISTANCE 400 // Maximum distance (in cm) to ping.
+#define PING_INTERVAL 33 // Milliseconds between sensor pings (29ms is about the min to avoid cross-sensor echo).
 
-int min_degree = 40;
-int max_degree = 120;
+//Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN, NEO_GRB + NEO_KHZ800);
 
-int pos = 90;    // variable to store the servo position
-unsigned long lastUpdate; // last update of position
+String mode = "stop";
+int pos = 0;    // variable to store the servo position
+unsigned long ping_current_millis;
 
-// interval between updates
-int updateInterval = 50;
-int update_interval = updateInterval;
-int increment = 2;
+// defaults to stop
+int incomingByte = 115;
 
-// This seems to be smooths than above intervals and increments
-// interval between updates
-//int  updateInterval = 18;
-//int increment = 1;
+// Mega trigger
+int triggerPin = 37;
 
-// 
-// == Ultrasonic
-//
-
-const int anPin0 = A0;
-const int anPin1 = A1;
-const int anPin2 = A2;
-const int anPin3 = A3;
-const int anPin4 = A4;
-const int anPin5 = A5;
-const int anPin6 = A6;
-const int anPin7 = A7;
-const int anPin8 = A8;
-const int anPin9 = A9;
-const int anPin10 = A10;
-const int anPin11 = A11;
-const int anPin12 = A12;
-const int anPin13 = A13;
-const int anPin14 = A14;
-const int anPin15 = A15;
-
-long distance0;
-long distance1;
-long distance2;
-long distance3;
-long distance4;
-long distance5;
-long distance6;
-long distance7;
-long distance8;
-long distance9;
-long distance10;
-long distance11;
-long distance12;
-long distance13;
-long distance14;
-long distance15;
-
-long tmp_dist;
-
-// When we setup the NeoPixel library, we tell it how many pixels, and which pin to use to send signals.
-// Note that for older NeoPixel strips you might need to change the third parameter--see the strandtest
-// example for more information on possible values.
-Adafruit_NeoPixel pixels = Adafruit_NeoPixel(NUMPIXELS, PIN);
-
-int delayval = 2; // delay for half a second
-
-unsigned long current_millis = 0;
-unsigned long lastSensorUpdate;
-unsigned long updateSensorInterval = 50;
-
-int map_val1, map_val2;
-
-// sensor read millis sudo delay
-
+int update_interval = 18;
+int increment = 1;
 
 class Enhancer
 {
-  SimplexNoise sn;
+  // Constructor
+//  SimplexNoise sn;
+  // create servo object to control a servo
+  Adafruit_TiCoServo *servo;
+//  Servo servo;
 
   unsigned long current_millis;
-  int pin_cache;
-  int min_degree = 0;
-  int max_degree = 0;
-  
-  boolean noise_interact_triggered = false;
-  int noise_trigger_pos = 0;
-  boolean sweep_react_pause_triggered = false;
-  int sweep_react_pause_pos = 0;
 
+  int pin_cache;
+  int min_degree = 40;
+  int max_degree = 120;
+  
   double n;
   float increase = 0.01;
   float x = 0.0;
   float y = 0.0;
 
-  int minAngle = 10;
-  int maxAngle = 170;
-
-  // create servo object to control a servo
-  Adafruit_TiCoServo servo;
   int pos;              // current servo position
   int increment;        // increment to move for each interval
-  int  updateInterval;      // interval between updates
+  int updateInterval;      // interval between updates
   unsigned long lastUpdate; // last update of position
-  NewPing *sonar;
+  int sonar;
   int currentDistance;
   int id;
-  int lowPos;
-  int highPos;
-  int lowDistance;
-  int highDistance;
-
-  String sweepString = "";
 
   unsigned long pausedPreviousMillis;
   unsigned long pausedInterval;
@@ -173,38 +112,68 @@ class Enhancer
   unsigned long pauseRepopulateInterval;
   bool pausedRepopulate;
 
+  boolean printJSON = true;
+  boolean publish_data = false;
+
   // number of pings collected
   unsigned long pingTotalCount = 0;
   // number of pings before send for simplexNoise
   // unsigned long pingRemainderValue = 50;
   unsigned long pingRemainderValue = 10;
 
-  // this section is for interaction smoothing
-  //===========================
-  static const int numReadings = 5;
-  // the readings from the analog input
-  int readings[numReadings];
-  // the index of the current reading
-  int readIndex = 0;
-  // the running total
-  int total = 0;
-  // the average
-  int average = 0;
-  // END ===========================
+  int distance;
+//  int anPin;
 
   public:
-    Enhancer(int ide, int interval, const int pinSonar, int position, String mode, unsigned long pcount)
+    Enhancer(Adafruit_TiCoServo servo, int ide, int interval, int sonr, int position, String mode, unsigned long pcount, int incmt)
     {
+
+//      servo = theServo;
       
+      mode = mode;
+      updateInterval = interval;
+      // makes sure the ID never gets out of the number of objects
+      id = constrain(ide, 0, 15);
+      pos = position;
+      increment = incmt;
+      paused = false;
+      sonar = sonr;
+      
+      // sets the pingcount offset so it doesn't send all of the scanned data through serial at once.
+      pingTotalCount = pcount;
+
+      pausedPreviousMillis = 0;
+      pausedInterval = 2000;
+
+      // this is for that pause for repopulating the data acter an avoidance
+      pauseRepopulateDistanceMeasurementMillis = 0;
+      pauseRepopulateInterval = 2000;
+      pausedRepopulate = true;
+    
+      // sets the noise x pos randomly to prevent objects moving in the same pattern
+      x = random(0.0, 20.0);      
     }
 
+    // Not sure if this needs to be individually, but more as an enviroment as a whole.
+    // One trigger pin on setup and as a start sensor...
+    // Ref multi_sensor_test for example...
+    void StartSensor(int pin)
+    {
+//      digitalWrite(triggerPin,HIGH);
+//      delay(1);
+//      digitalWrite(triggerPin,LOW);
+    }
     
     void Attach(int pin)
     {
+
       // if it is not attached, attach
       // otherwise don't try and re-attach
-      if(servo.attached() == 0){
-        servo.attach(pin); 
+      if(servo->attached() == 0){
+
+//        Serial.print("Attache pin: ");
+//        Serial.println(pin);
+        servo->attach(pin); 
       }
 
       // cache the pin ID in constructure to use later for 
@@ -212,27 +181,27 @@ class Enhancer
       if(!pin_cache){
         PinCache(pin);
       }
+      
     }
 
-    void PinCache (int pin)
-    {
+    void PinCache (int pin){
       pin_cache = pin;
     }
 
     void Detach()
     {
-      servo.detach();
+      servo->detach();
     }
 
     void SetPos(int startPosition)
     {
       pos = startPosition;
-      servo.write(pos);
+      servo->write(pos);
     }
   
     int isAttached()
     {
-      return servo.attached();
+      return servo->attached();
     }
 
     void switchIncrementDirection()
@@ -242,11 +211,10 @@ class Enhancer
 
     void GoTo(int pos)
     {
-      servo.write(pos);
+      servo->write(pos);
     }
 
-    void resetDefaults()
-    {
+    void resetDefaults(){
       increment = 2;
       lastUpdate = millis();
       paused = false;
@@ -254,191 +222,231 @@ class Enhancer
       pausedPreviousMillis = millis();
     }
 
-    void Update() {
-
-    }
-
     void setMode (String md){
       mode = md;
     }
-};
+
+    void PrintDistance(int d)
+    {
+      Serial.print("Print Distance: ");
+      Serial.println(d);
+      // Serial.println(sonar->ping_result);
+      Serial.println("===================");
+    }
+
+    void Update() {
+
+//      Serial.print("In Update, ID: ");
+//      Serial.println(id);
+
+      if (pos == -1) {
+        pos = 0;
+        servo->write(pos);
+      }
+
+      current_millis = millis();
+
+      // pause after reset to gather new distance measurements
+
+//      if ((current_millis - pauseRepopulateDistanceMeasurementMillis) > pauseRepopulateInterval) {
+//        pausedRepopulate = false;
+//      }
       
-// Number of sensors.
-#define OBJECT_NUM  16
+      if (mode == "sweep") {
+
+        if((current_millis - lastUpdate) > updateInterval)  // time to update
+        {
+          lastUpdate = millis();
+          pos += increment;
+    
+          // 
+          // =================
+          Attach(pin_cache);
+          servo->write(pos);
+          delay(2);
+
+          // sweep
+          // == BEGINNING OF SWEEP SEND DATA
+          if ((pos >= max_degree) || (pos <= min_degree)) // end of sweep
+          {
+            Detach();
+            servo->attach(pin_cache);
+            // reverse direction
+            increment = -increment;
+          }
+          // END OF SWEEP SEND DATA
+
+        }
+
+      }
+
+    }
+
+    void readSensor() {
+      // #TODO: update values.. for this class here
+      distance = analogRead(sonar)/2;
+    }
+
+    int getSensorReading() {
+      return distance;
+    }
+};
+
+#define OBJECT_NUM  2
 
 unsigned long pingTimer[OBJECT_NUM]; // Holds the times when the next ping should happen for each sensor.
 unsigned int cm[OBJECT_NUM];         // Where the ping distances are stored.
 uint8_t currentSensor = 0;          // Keeps track of which sensor is active.
 
 // Sensor object array.
-// ID, Update Interval, Sonar Pin ID, Start Possition, mode, ping index offset
-Enhancer enhance[OBJECT_NUM] = {
-  Enhancer(0, update_interval, anPin0, 90, mode, 0),
-  Enhancer(1, update_interval, anPin1, 90, mode, 2),
-  Enhancer(2, update_interval, anPin2, 90, mode, 4),
-  Enhancer(3, update_interval, anPin3, 90, mode, 6),
-  Enhancer(4, update_interval, anPin4, 90, mode, 8),
-  Enhancer(5, update_interval, anPin5, 90, mode, 10),
-  Enhancer(6, update_interval, anPin6, 90, mode, 12),
-  Enhancer(7, update_interval, anPin7, 90, mode, 14),
-  Enhancer(8, update_interval, anPin8, 90, mode, 16),
-  Enhancer(9, update_interval, anPin9, 90, mode, 18),
-  Enhancer(10, update_interval, anPin10, 90, mode, 20),
-  Enhancer(11, update_interval, anPin11, 90, mode, 22),
-  Enhancer(12, update_interval, anPin12, 90, mode, 24),
-  Enhancer(13, update_interval, anPin13, 90, mode, 26),
-  Enhancer(14, update_interval, anPin14, 90, mode, 28),
-  Enhancer(15, update_interval, anPin15, 90, mode, 30)
+// Each sensor's analog pin, trigger pin, and max distance to ping.
+int sonar[OBJECT_NUM][3] = {
+  {A0, triggerPin, 800},
+  {A1, triggerPin, 800}
 };
 
+Adafruit_TiCoServo servo0;
+Adafruit_TiCoServo servo1;
 
+// Sensor object array.
+// ID, Update Interval, Sonar ID, Start Possition, mode, ping index offset, increment
+Enhancer enhance[OBJECT_NUM] = {
+  Enhancer(servo0, 0, update_interval, sonar[0], 90, mode, 0, increment),
+  Enhancer(servo1, 1, update_interval, sonar[1], 90, mode, 2, increment)
+};
 
 void setup() {
-  Serial.begin(9600);  // sets the serial port to 9600
+  Serial.begin(115200);
 
-//  servo.attach(9);  // attaches the servo on pin 9 to the servo object
-//  servo2.attach(10);  // attaches the servo on pin 9 to the servo object
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
 
-  // This is for Trinket 5V 16MHz, you can remove these three lines if you are not using a Trinket
-  #if defined (__AVR_ATtiny85__)
-    if (F_CPU == 16000000) clock_prescale_set(clock_div_1);
-  #endif
-    // End of trinket special code
-    
-  pixels.begin(); // This initializes the NeoPixel library.
+  // First ping starts at 75ms, gives time for the Arduino to chill before starting.
+  pingTimer[0] = millis() + 75;
+  // Set the starting time for each sensor.
+  for (uint8_t i = 1; i < OBJECT_NUM; i++) {
+    pingTimer[i] = pingTimer[i - 1] + PING_INTERVAL;
+  }
+
+  Serial.println("In Setup");
 
   massAttatch();
-
   establishContact();
+}
+
+void sensorSetup(){
+  pinMode(triggerPin, OUTPUT);
 }
 
 void establishContact() {
   while (Serial.available() <= 0) {
     // send a capital A
     // Serial.println('A');
-    delay(300);
+//    delay(300);
   }
-}
-
-void read_sensors(){
-  /*
-  Scale factor is (Vcc/512) per inch. A 5V supply yields ~9.8mV/in
-  Arduino analog pin goes from 0 to 1024, so the value has to be divided by 2 to get the actual inches
-  */
-
-  if((current_millis - lastSensorUpdate) > updateSensorInterval)  // time to update
-  {
-    lastSensorUpdate = millis();
-    distance1 = analogRead(anPin1)/2;
-    distance2 = analogRead(anPin2)/2;
-  }
-}
-
-void print_all(){
-  Serial.print("S1");
-  Serial.print(" ");
-  Serial.print(distance1);
-  Serial.print(" inches");
-  Serial.print(" || ");
-  Serial.print("S2");
-  Serial.print(" ");
-  Serial.print(distance2);
-  Serial.print(" inches");
-  Serial.println();
 }
 
 void loop() {
-
-  current_millis = millis();
   
-  read_sensors();
-//  print_all();
+  // read the incoming byte:
+  if (Serial.available() > 0) {
+    incomingByte = Serial.read();
+    Serial.println("Character: " + incomingByte);
+  }
 
-  // For a set of NeoPixels the first NeoPixel is 0, second is 1, all the way up to the count of pixels minus one.
+    // Different Key Codes
+  // =================
+  
+  // Go
+  // g = 103
 
-  if((current_millis - lastUpdate) > updateInterval)  // time to update
-  {
-    lastUpdate = millis();
-    pos += increment;
-    if ((pos >= max_degree) || (pos <= min_degree)) // end of sweep
-    {
-      // reverse direction
-      increment = -increment;
-    }
+  // Stop
+  // s = 115
+
+  // Next
+  // n = 110
+
+  // Previous
+  // p = 112
+
+  // Configure
+  // c = 99
+
+  // Mode Controles + hex values
+  // 1 = Sweep / 49
+  // 2 = sweep react / 50
+  // 3 = sweep react pause / 51
+  // 4 = noise / 52
+  // 5 = noise react / 53
+  // 6 = pattern_wave_small_v2: smaller wave form / 54
+  // 7 = measure / 55
+  // 8 = measure + react only / 56
+
+  // END OF CODES
+  // ==================
+
+  
+  // go
+  if (incomingByte == 103) {
     
-    servo.write(pos);
-    servo2.write(pos);
-  }
+    massAttatch();
+    mode = "sweep";
   
-  if(pos % 10 == 0){
-    map_val1 = map(distance1, 0, 350, 0, 250);
-    map_val2 = map(distance2, 0, 350, 0, 250);
-
-    Serial.print(map_val1);
-    Serial.print(" inches");
-
-    Serial.print(" || ");
-      
-    Serial.print(map_val2);
-    Serial.print(" inches");
-    Serial.println();
-        
-    for(int i=0;i<NUMPIXELS;i++){
-  
-      if(i < 30){
-        pixels.setPixelColor(i, pixels.Color(map_val1, map_val1, map_val1));            
-      } else {
-        pixels.setPixelColor(i, pixels.Color(map_val2, map_val2, map_val2));
-      }  
+  // stop
+  } else if (incomingByte == 115) {
+    mode = "stop";
+    pos = 90;
+    for (uint8_t i = 0; i < OBJECT_NUM; i++) {
+      enhance[i].GoTo(pos);
+      enhance[i].resetDefaults();
     }
+//    delay(500);
+    massDetatch();
+    return;
+
+  // resets position back to 90
+  // 'c'
+  } else if (incomingByte == 99) {
+    pos = 90;
+    for (uint8_t i = 0; i < OBJECT_NUM; i++) {
+      enhance[i].GoTo(pos);
+      enhance[i].resetDefaults();
+    }
+    return;
+
+  // Sweep
+  } else if (incomingByte == 49) {
+    mode = "sweep";
+    for (uint8_t i = 0; i < OBJECT_NUM; i++) {
+     enhance[i].setMode(mode);
+     enhance[i].resetDefaults();
+    }
+  }
   
-    pixels.show(); // This sends the updated pixel color to the hardware.
-  }
+  incomingByte = 0;
 
-  delay(delayval);
-}
-
-// Input a value 0 to 255 to get a color value.
-// The colours are a transition r - g - b - back to r.
-uint32_t Wheel(byte WheelPos) {
-  WheelPos = 255 - WheelPos;
-  if(WheelPos < 85) {
-    return pixels.Color(255 - WheelPos * 3, 0, WheelPos * 3);
-  }
-  if(WheelPos < 170) {
-    WheelPos -= 85;
-    return pixels.Color(0, WheelPos * 3, 255 - WheelPos * 3);
-  }
-  WheelPos -= 170;
-  return pixels.Color(WheelPos * 3, 255 - WheelPos * 3, 0);
-}
-
-// detatch all servos
-void massDetatch() {
-  // detatch all motors to save energy / motor life span
-  // Set the starting time for each sensor.
   for (uint8_t i = 0; i < OBJECT_NUM; i++) {
-    sweep[i].Detach();
+//    Serial.println("In Object Loop!");
+    enhance[i].Update();
   }
 }
 
 void massAttatch() {
+
+  Serial.println("In Attach");
   // Attach all motors
-  enhance[0].Attach(A0);
-  enhance[1].Attach(A1);
-  enhance[2].Attach(A2);
-  enhance[3].Attach(A3);
-  enhance[4].Attach(A4);
-  enhance[5].Attach(A5);
-  enhance[6].Attach(A6);
-  enhance[7].Attach(A7);
-  enhance[8].Attach(A8);
-  enhance[9].Attach(A9);
-  enhance[10].Attach(40);
-  enhance[11].Attach(38);
-  enhance[12].Attach(36);
-  enhance[13].Attach(34);
-  enhance[14].Attach(32);
-  enhance[15].Attach(30);
+  enhance[0].Attach(2);
+  enhance[1].Attach(3);
+}
+
+// detatch all servos
+void massDetatch() {
+  Serial.println("In DETATCH");
+  // detatch all motors to save energy / motor life span
+  // Set the starting time for each sensor.
+  for (uint8_t i = 0; i < OBJECT_NUM; i++) {
+    enhance[i].Detach();
+  }
 }
 
